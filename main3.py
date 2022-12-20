cc4d26a29c0a6a4fb14c47205ad938597b9376b0
Anton Tseitlin <antontseytlin@gmail.com>
2022-12-20 20:00:52 +0100

    New lesson(3)

---
+++ main3.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+photo = np.zeros((450,450, 3), dtype='uint8')
+
+# RGB - стандарт
+# BGR - формат в OpenCV
+#photo[100:150,200:280 ] = 119,201,105
+
+cv2.rectangle(photo, (40,40),(100,100),(119,201,105), thickness=cv2.FILLED)
+
+cv2.line(photo, (0, photo.shape[0] // 2), (photo.shape[1],photo.shape[1] // 2),(119,201,105), thickness=3)
+
+cv2.circle(photo, (photo.shape[0]//2,photo.shape[1]//2),50,(119,201,105),thickness=cv2.FILLED)
+
+cv2.putText(photo,'itProger',(100,150),cv2.FONT_HERSHEY_TRIPLEX, 1,(255,0,0),1)
+
+cv2.imshow('Photo',photo)
+cv2.waitKey(0)
